6649e23ce4fd9a493228ada47eab0ba2cd17d531
simrantyagi <86233722+simrantyagi@users.noreply.github.com>
2021-06-28 18:11:13 +0530

    Update cp.py

--- cp.py
+++ cp.py
@@ -1,9 +1,7 @@
 import pathlib
 import argparse
 import shutil
-from future.moves import tkinter as Tk
-from tkinter import *
-from tkinter import messagebox
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Copy directory")
     parser.add_argument('source')
@@ -13,9 +11,7 @@ if __name__ == '__main__':
     if file.exists():
         shutil.copytree(args.source, args.destination, dirs_exist_ok=True)
     else:
-        top = Tk()
-        top.geometry("100x100")
-        messagebox.showerror("error","Source Directory not exists")
+        print(" Directory not exist")
 
 
 
